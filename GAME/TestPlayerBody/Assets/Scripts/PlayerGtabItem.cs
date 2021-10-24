a8d1561175410c69310c0f3441972321319e1295
Sirinya-K <sirinya.kamyan@hotmail.com>
2021-10-25 02:06:22 +0700

    [VR] Modify Grab class and add Release function

--- GAME/TestPlayerBody/Assets/Scripts/PlayerGtabItem.cs
+++ GAME/TestPlayerBody/Assets/Scripts/PlayerGtabItem.cs
@@ -6,81 +6,110 @@ public class PlayerGtabItem : MonoBehaviour
 {
     private LayerMask grabbableLayer;
 
-    private Rigidbody connectedRigidbody;
+    private Rigidbody connectedBody, itemBody;
+
     private FixedJoint itemJoint;
 
-    [SerializeField] private GameObject indexCollider;
-    [SerializeField] private GameObject middleCollider;
+    [SerializeField]
+    private GameObject indexCollider;
+
+    [SerializeField]
+    private GameObject middleCollider;
 
     private FingerCollision indexCollision, middleCollision;
 
     private PlayerHandController hand;
 
-    [SerializeField] private GameObject topPalm;
-    [SerializeField] private GameObject bottomPalm;
-    private Rigidbody topPalmRigidbody, bottomPalmRigidbody;
-    private FixedJoint itemJoint1, itemJoint2;
+    private string finger = "";
+    private float fingerValue = 0f;
+    private float diff = 0.1f;
 
     // Start is called before the first frame update
     void Start()
     {
         grabbableLayer = LayerMask.NameToLayer("Grabbable");
 
-        connectedRigidbody = GetComponent<Rigidbody>();
+        connectedBody = GetComponent<Rigidbody>();
 
         indexCollision = indexCollider.GetComponent<FingerCollision>();
         middleCollision = middleCollider.GetComponent<FingerCollision>();
 
         hand = GameObject.FindObjectOfType<PlayerHandController>();
-
-        topPalmRigidbody = topPalm.GetComponent<Rigidbody>();
-        bottomPalmRigidbody = bottomPalm.GetComponent<Rigidbody>();
     }
 
     private void OnCollisionStay(Collision other)
     {
-        if(other.gameObject.layer == grabbableLayer)
+        if (other.gameObject.layer == grabbableLayer)
         {
-            if(other.gameObject.GetComponent<FixedJoint>() == null)
+            if (other.gameObject.GetComponent<FixedJoint>() == null)
             {
                 // Check that Index or Middle collision with the item
-                if((indexCollision.collisionStatus == true && hand.RIndexValue >= 0.15) || (middleCollision.collisionStatus == true && hand.RMiddleValue >= 0.15))
+                if ((indexCollision.collisionStatus == true && hand.RIndexValue >= 0.15) || (middleCollision.collisionStatus == true && hand.RMiddleValue >= 0.15))
                 {
                     Debug.Log("Grab");
 
                     // Create Fixed Joint between the Item and Hand
-                    var itemBody = other.gameObject.GetComponent<Rigidbody>();
+                    itemBody = other.gameObject.GetComponent<Rigidbody>();
                     itemBody.collisionDetectionMode = CollisionDetectionMode.Continuous;
                     itemBody.interpolation = RigidbodyInterpolation.Interpolate;
-                    
+
                     itemJoint = other.gameObject.AddComponent<FixedJoint>();
-                    itemJoint.connectedBody = connectedRigidbody;
+                    itemJoint.connectedBody = connectedBody;
                     itemJoint.connectedMassScale = 0;
 
                     // Set the Item's gravity to false
                     itemBody.useGravity = false;
 
-                    // Add CheckVelocity Script to thr Item
+                    // Add CheckVelocity Script to the Item
                     other.gameObject.AddComponent<CheckVelocity>();
 
-                    // itemJoint1 = other.gameObject.AddComponent<FixedJoint>();
-                    // itemJoint1.connectedBody = topPalmRigidbody;
-                    // itemJoint1.connectedMassScale = 0;
-
-                    // itemJoint2 = other.gameObject.AddComponent<FixedJoint>();
-                    // itemJoint2.connectedBody = bottomPalmRigidbody;
-                    // itemJoint2.connectedMassScale = 0;
-                }   
+                    // Save Finger Value
+                    if (indexCollision.collisionStatus && middleCollision.collisionStatus)
+                    {
+                        finger = "both";
+                        fingerValue = hand.RMiddleValue;
+                    }
+                    else if (indexCollision.collisionStatus)
+                    {
+                        finger = "index";
+                        fingerValue = hand.RIndexValue;
+                    }
+                    else if (middleCollision.collisionStatus)
+                    {
+                        finger = "middle";
+                        fingerValue = hand.RMiddleValue;
+                    }
+                }
             }
         }
     }
 
-    private void OnCollisionExit(Collision other) {
+    private void Release()
+    {
+        Debug.Log("Release");
+        
+        Destroy(itemJoint);
+        itemBody.useGravity = true;
+        itemBody = null;
+
+        finger = "";
+        fingerValue = 0f;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-
+        if (finger == "index" && (fingerValue - hand.RIndexValue >= diff))
+        {
+            Release();
+        }
+        else if(finger == "middle" && (fingerValue - hand.RMiddleValue >= diff))
+        {
+            Release();
+        }
+        else if(finger == "both" && (fingerValue - hand.RIndexValue >= diff && fingerValue - hand.RMiddleValue >= diff))
+        {
+            Release();
+        }
     }
 }
