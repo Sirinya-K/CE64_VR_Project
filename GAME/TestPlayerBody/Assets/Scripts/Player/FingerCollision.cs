1a0a28c2ff76964590b29a70be49d2b9bbe20a62
Sirinya-K <sirinya.kamyan@hotmail.com>
2022-03-25 20:30:41 +0700

    [VR] Fix Traning Dummy

---
+++ GAME/TestPlayerBody/Assets/Scripts/Player/FingerCollision.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FingerCollision : MonoBehaviour
+{
+    public PlayerHandController hand;
+
+    private LayerMask grabbableLayer;
+    private LayerMask furniture;
+
+    [HideInInspector] public bool collisionStatus;
+
+    private void Start()
+    {
+        grabbableLayer = LayerMask.NameToLayer("Grabbable");
+        furniture = LayerMask.NameToLayer("Furniture");
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.layer == grabbableLayer || other.gameObject.layer == furniture)
+        {
+            // Debug.Log(gameObject.name + " + " + other.gameObject.name + " --> Enter");
+            collisionStatus = true;
+
+            hand.PublishHandToMqtt("stop", gameObject.name);
+
+            Debug.Log(this.name + " --> " + other.gameObject.name);
+        }
+    }
+
+    private void OnCollisionExit(Collision other)
+    {
+        if (other.gameObject.layer == grabbableLayer || other.gameObject.layer == furniture)
+        {
+            // Debug.Log(gameObject.name + " + " + other.gameObject.name + " --> Exit");
+            collisionStatus = false;
+
+            hand.PublishHandToMqtt("free", gameObject.name);
+        }
+    }
+}
